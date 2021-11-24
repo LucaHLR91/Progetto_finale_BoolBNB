f80c77123ebc76e2cdcc1b4532d7b22fcf6d777a
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-11-24 16:00:28 +0100

    Popolazione tabelle Users e Apartments

---
+++ database/seeds/ApartmentsTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Apartment;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+use Faker\Factory as FakerFactory;
+
+
+class ApartmentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        $faker = FakerFactory::create('it_IT');
+        for($i = 0; $i < 3; $i++) {
+            $new_apartment = new Apartment();
+            $new_apartment->title = $faker->sentence(3);
+            $new_apartment->beds = $faker->numberBetween(1, 5);
+            $new_apartment->rooms = $faker->numberBetween(1, 5);
+            $new_apartment->bathrooms = $faker->numberBetween(1, 3);
+            $new_apartment->square_meters= $faker->numberBetween(40, 120);
+            $new_apartment->image= $faker->image(null, 640, 480, 'house', true, true, null);
+            $new_apartment->avaliability= $faker->boolean();
+            $new_apartment->address = $faker->address();
+            $new_apartment->city= 'Milano';
+            $new_apartment->latitude= $faker->latitude(45, 46);
+            $new_apartment->longitude= $faker->longitude(9, 10);
+            $new_apartment->slug= Str::slug($new_apartment->title, '-');
+            $new_apartment->save();
+        }
+    }
+}
+
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Faker\Factory as FakerFactory;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        $faker = FakerFactory::create('it_IT');
+        for($i = 0; $i < 3; $i++) {
+            $new_user = new User;
+            $new_user->name = $faker->name();
+            $new_user->surname= $faker->lastName();
+            $new_user->date_of_birth= $faker->dateTimeBetween('-70 years', '-18 years');
+            $new_user->email= $faker->safeEmail();
+            $new_user->password= $faker->password(8, 20);
+            $new_user->save();
+        }
+    }
+}
