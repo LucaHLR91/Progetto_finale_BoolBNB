154cb3a53fe0d8436708de215b8e02383be6bf5f
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-11-24 09:55:03 +0100

    Fillables

--- app/Apartment.php
+++ app/Apartment.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Apartment extends Model
 {
-    //
+    protected $fillable = [
+        'title', 'beds', 'rooms', 'bathrooms', 'square_meters', 'image', 'avaliability', 'address', 'city', 'latitude', 'longitude', 'slug',
+    ];
 }
