c6b0f9e12a2f67b3851c7c01d047d5d36467da7f
Vlad <andreivladpaul@gmail.com>
2021-11-26 17:35:50 +0100

    correzione indentazione show.blade

--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -3,33 +3,33 @@
 @section('content')
 
 
-<div class="container ">
-    <div class="row">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">Nome Appartamento</th>
-                    <th scope="col">Servizi</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>
-                        {{ $apartment->title }}
-                    </td>
-                    <td>
-                        @foreach ($apartment->services as $service)
-                            <p>{!! $service->service_name !!}</p>
-                        @endforeach
-                    </td>
+    <div class="container ">
+        <div class="row">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Nome Appartamento</th>
+                        <th scope="col">Servizi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            {{ $apartment->title }}
+                        </td>
+                        <td>
+                            @foreach ($apartment->services as $service)
+                                <p>{!! $service->service_name !!}</p>
+                            @endforeach
+                        </td>
 
-                </tr>
+                    </tr>
 
-            </tbody>
-        </table>
+                </tbody>
+            </table>
+        </div>
+
     </div>
-
-</div>
 
 @endsection
 
