2b72abca1f36b64aef673acfdd1b8ceeac111eda
Edoardo <ercolino.edoardo@gmail.com>
2021-11-28 17:11:23 +0100

    prima parte grafica dei layouts

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -25,57 +25,59 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark flex-md-nowrap p-0">
-        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">BoolBNB</a>
-        <ul class="navbar-nav px-3 ml-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="/">
-                    Visita il sito
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/"
-                   onclick="event.preventDefault();
-                                 document.getElementById('logout-form').submit();">
-                    Logout
-                </a>
-                <form id="logout-form" action="admin/logout" method="POST" style="display: none;">
-                    @csrf
-                </form>
-            </li>
-        </ul>
-    </nav>
-    <div class="container-fluid">
-        <div class="row">
-            <nav class="col-md-2 d-none d-md-block bg-light sidebar py-4">
-                <div class="sidebar-sticky">
-                    <ul class="nav flex-column">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="{{ route('admin.dashboard_home') }}">
-                                <i class="fas fa-digital-tachograph"></i>
-                                Dashboard
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('admin.apartments.index') }}">
-                                <i class="fas fa-list-ol"></i>
-                                Miei Appartamenti
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('admin.apartments.create') }}">
-                                <i class="fas fa-plus"></i>
-                                Inserisci Appartamento
-                            </a>
-                        </li>
-                    </ul>
-
-                </div>
-            </nav>
-
-            <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4 py-4">
-                @yield('content')
-            </main>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-dark my-navbar flex-md-nowrap p-0">
+            <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">BoolBNB</a>
+            <ul class="navbar-nav px-3 ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="/">
+                        Visita il sito
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/"
+                       onclick="event.preventDefault();
+                                     document.getElementById('logout-form').submit();">
+                        Logout
+                    </a>
+                    <form id="logout-form" action="admin/logout" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </li>
+            </ul>
+        </nav>
+        <div class="container-fluid">
+            <div class="row">
+                <nav class="col-md-2 d-none d-md-block my-dashboard sidebar py-4">
+                    <div class="sidebar-sticky">
+                        <ul class="nav flex-column">
+                            <li class="nav-item">
+                                <a class="nav-link active" href="{{ route('admin.dashboard_home') }}">
+                                    <i class="fas fa-digital-tachograph"></i>
+                                    Dashboard
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('admin.apartments.index') }}">
+                                    <i class="fas fa-list-ol"></i>
+                                    Miei Appartamenti
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('admin.apartments.create') }}">
+                                    <i class="fas fa-plus"></i>
+                                    Inserisci Appartamento
+                                </a>
+                            </li>
+                        </ul>
+    
+                    </div>
+                </nav>
+    
+                <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4 py-4">
+                    @yield('content')
+                </main>
+            </div>
         </div>
     </div>
 </body>
