0853dd5dfb20c80ae9c7a4964f01f08665de6896
Edoardo <ercolino.edoardo@gmail.com>
2021-11-29 15:21:52 +0100

    fix web app

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -72,7 +72,7 @@
                     </div>
                 </nav>
 
-                <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4 py-4 full-height ">
+                <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4 py-4 full-height overflow">
                     @yield('content')
                 </main>
             </div>
