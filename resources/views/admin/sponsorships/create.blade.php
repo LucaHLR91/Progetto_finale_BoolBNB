293e37412352797c442beeedc88c16524035c743
robertoAureli <r.aureli@protonmail.com>
2021-11-30 10:14:02 +0100

    View create sponsorship

--- resources/views/admin/sponsorships/create.blade.php
+++ resources/views/admin/sponsorships/create.blade.php
@@ -17,22 +17,17 @@
 
     <div class="form-group">
         <p>Scegli il tipo di sponsorizzazione :</p>
-        
+        <input type="hidden" name="id_appartamento" value="{{$apartment['id']}}">
         @foreach ($sponsorships as $sponsorship)
-        <div class="form-check form-check-inline">
-            <input {{ ($sponsorship['id']) }} value="{{ $sponsorship['id'] }}"
-                id="{{ 'boost_type' . $sponsorship['id'] }}" type="checkbox" name="sponsorship[]" class="form-check-input">
-            <label for="{{ 'sponsorship' . $sponsorship['id'] }}"
-                class="form-check-label">Tipo {{$sponsorship['id']}}  Durata {{$sponsorship['duration']}} Costo {{$sponsorship['price']}}</label>
-        </div>
+            <div class="form-check form-check-inline">
+                <input class="form-chek-input" value="{{ $sponsorship['id'] }}"
+                    id="{{ 'sponsorship' . $sponsorship['id'] }}" type="radio" name="sponsorship[]"
+                    class="form-check-input">
+                <label for="{{ 'sponsorship' . $sponsorship['id'] }}" class="form-check-label">Tipo {{$sponsorship['id']}}
+                    Durata {{$sponsorship['duration']}} Costo {{$sponsorship['price']}}</label>
+
+            </div>
         @endforeach
-
-
-        <div class="form-group">
-        <div style="background-color: white" id="dropin-container"></div>
-        <button id="submit-button">Request payment method</button>
-    </div>
-    </div>
 
     <button type="submit" class="btn btn-primary form-group">Sponsorizza</button>
 </form>
@@ -43,7 +38,7 @@
         container: '#dropin-container'
     }, function (createErr, instance) {
         button.addEventListener('click', function () {
-            console.log("test");
+            //
         });
     });
 
