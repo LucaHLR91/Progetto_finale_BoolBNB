7c7c238cae6bbc3f261cd38ef9256ba8f8324518
Edoardo <ercolino.edoardo@gmail.com>
2021-11-30 15:31:11 +0100

    Grafica homepage

--- resources/views/Guest/home.blade.php
+++ resources/views/Guest/home.blade.php
@@ -10,23 +10,38 @@
                 @csrf
                 @method('POST')
                 <div class="row">
-                    <div class="col-md-8">
+                    {{-- dove andare --}}
+                    <div class="col-md-3">
                         <label for="address" class="form-lable">Dove vuoi andare?</label>
                         <input type="search" id="address" name="address" class="form-control" placeholder="Indirizzo  Appartamento"/>
                     </div>
-                    <div class="col-md-4 d-flex flex-column">
+
+                    {{-- raggio --}}
+                    <div class="col-md-3 d-flex flex-column">
                         <label for="slider-range" class="form-lable">Raggio di ricerca: </label>
-                        <input id="slider-range" class="d-flex" name="range" type="range" min="0" max="100" value="20" oninput="this.nextElementSibling.value = this.value" />
-                        <output class="ml-2"> 20 </output>
-                        <span class="font-weight-bold ml-2">Km</span>
+                        <div class="d-felx">
+                            <input id="slider-range" class="mt-2" name="range" type="range" min="0" max="100" value="20" oninput="this.nextElementSibling.value = this.value" />
+                            <output class="ml-2"> 20 </output>
+                            <span class="font-weight-bold ml-2">Km</span>
+                        </div>
                     </div>
+
+                    {{-- stanze --}}
+                    <div class="col-md-3">
+                        <label for="room" class="form-lable">Numero stanze</label>
+                        <input type="number" id="room" name="room" class="form-control" placeholder="Inserisci N°stanze" min="0"/>
+                    </div>
+
+                    {{-- letti --}}
+                    <div class="col-md-3">
+                        <label for="bed" class="form-lable">Posti letto</label>
+                        <input type="number" id="bed" name="bed" class="form-control" placeholder="Inserisci N°letti" min="0"/>
+                    </div>
+
                 </div>
-                <div class="row d-flex justify-content-center">
-                    <div class="col-2">
+                <div class="row text-center pt-3">
+                    <div class="col-12">
                         <button type="submit" class="btn btn-primary form-group">Cerca</button>
-                    </div>
-                    <div class="col-2">
-                        <button type="submit" class="btn btn-primary form-group">Ricerca avanzata</button>
                     </div>
                 </div>
             </form>
