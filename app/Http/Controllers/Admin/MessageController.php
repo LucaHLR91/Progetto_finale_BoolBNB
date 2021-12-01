43fea1723146cd6f1a5c042caafa060de0a302b7
Vlad <andreivladpaul@gmail.com>
2021-12-01 16:15:17 +0100

    Aggiunta di Carbon in messagecontroller

--- app/Http/Controllers/Admin/MessageController.php
+++ app/Http/Controllers/Admin/MessageController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Admin\Carbon;
+use Carbon\Carbon;
 use App\Message;
 
 class MessageController extends Controller
