bb5df711e2b13073daa5c028c46f6f2b3f566985
Edoardo <ercolino.edoardo@gmail.com>
2021-12-01 11:29:13 +0100

    correzione testo nero

--- resources/views/admin/apartments/create.blade.php
+++ resources/views/admin/apartments/create.blade.php
@@ -3,14 +3,14 @@
  
  
 @section('content')
-<h1>Inserisci il tuo appartamento</h1>
+<h1 class="text-dark">Inserisci il tuo appartamento</h1>
  
-<form action="{{route('admin.apartments.store')}}" method="post">
+<form action="{{route('admin.apartments.store')}}" method="post" class="text-dark">
     @csrf
     @method('POST')
  
     <div class="mb-3 form-group">
-        <label for="title" class="form-label">Titolo</label>
+        <label for="title" class="form-label"><h4>Titolo</h4></label>
         <input name="title" type="text" class="form-control" id="title" class="form-control @error('title') is-invalid @enderror"
             placeholder="Inserisci titolo" value="{{ old('title')}}">
         @error('title')
@@ -19,7 +19,7 @@
     </div>
  
     <div class="mb-3 form-group">
-        <label for="beds" class="form-label">Numero Letti</label>
+        <label for="beds" class="form-label"><h4>Numero Letti</h4></label>
         <input name="beds" type="number" class="form-control" id="beds" class="form-control @error('beds') is-invalid @enderror"
             placeholder="Inserisci il numero dei posti letto" value="{{ old('beds') }}">
         @error('beds')
@@ -28,7 +28,7 @@
     </div>
  
     <div class="mb-3 form-group">
-        <label for="rooms" class="form-label">Numero Stanze</label>
+        <label for="rooms" class="form-label"><h4>Numero Stanze</h4></label>
         <input name="rooms" type="number" class="form-control" id="rooms" placeholder="Inserisci il numero di stanze"
             class="form-control @error('rooms') is-invalid @enderror" value="{{ old('rooms') }}">
         @error('rooms')
@@ -37,7 +37,7 @@
     </div>
  
     <div class="mb-3 form-group">
-        <label for="bathrooms" class="form-label">Numero Bagni</label>
+        <label for="bathrooms" class="form-label"><h4>Numero Bagni</h4></label>
         <input name="bathrooms" type="number" class="form-control" id="bathrooms"
             placeholder="Inserisci il numero dei bagni" class="form-control @error('bathrooms') is-invalid @enderror" value="{{ old('bathrooms') }}">
          @error('bathrooms')
@@ -46,7 +46,7 @@
     </div>
  
     <div class="mb-3 form-group">
-        <label for="square_meters" class="form-label">Metri Quadri</label>
+        <label for="square_meters" class="form-label"><h4>Metri Quadri</h4></label>
         <input name="square_meters" type="number" class="form-control" id="square_meters"
             placeholder="Inserisci i metri quadri" class="form-control @error('square_meters') is-invalid @enderror" value="{{ old('square_meters') }}">
         @error('square_meters')
@@ -55,7 +55,7 @@
     </div>
  
     <div class="mb-3 form-group">
-        <label for="image" class="form-label">Foto</label>
+        <label for="image" class="form-label"><h4>Foto</h4></label>
         <input name="image" type="file" class="form-control" class="form-control @error('image') is-invalid @enderror" id="image">
         @error('image')
         <div class="alert alert-danger">{{ $message }}</div>
@@ -63,7 +63,7 @@
     </div>
  
     <div class="form-group">
-        <p>Disponibilità:</p>
+        <h4>Disponibilità:</h4>
         <div class="form-check">
             <input class="form-check-input" type="radio" name="avaliability" id="available" value="1" checked>
             <label class="form-check-label" for="available">
@@ -83,7 +83,7 @@
  
  
     <div class="mb-3 form-group">
-        <label for="address" class="form-label">Indirizzo</label>
+        <label for="address" class="form-label"><h4>Indirizzo</h4></label>
         <input name="address" type="text" id="address" placeholder="Inserisci l'indirizzo"
            class="form-control  @error('address') is-invalid @enderror" value="{{ old('address') }}">
         @error('address')
@@ -92,7 +92,7 @@
     </div>
  
     <div class="mb-3 form-group">
-        <label for="city" class="form-label">Città</label>
+        <label for="city" class="form-label"><h4>Città</h4></label>
         <input name="city" type="text" class="form-control" id="city" placeholder="Inserisci la città"
             class="form-control  @error('city') is-invalid @enderror" value="{{ old('city') }}">
          @error('city')
@@ -101,7 +101,7 @@
     </div>
  
     <div class="form-group">
-        <p>Scegli i servizi:</p>
+        <h4>Scegli i servizi:</h4>
         @foreach ($services as $service)
         <div class="form-check form-check-inline">
             {{-- services[] coterrà tutti i valori che noi selezioneremo --}}
