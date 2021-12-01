125e8315aaa4e2ec270703e40b526f71283eff6b
Ivan <ivan.galanti@live.it>
2021-12-01 17:22:57 +0100

    Carousel

--- resources/views/Guest/home.blade.php
+++ resources/views/Guest/home.blade.php
@@ -49,7 +49,45 @@
                 </div>
             </form>
         </div>  
-
+        
+        <div id="house_carousel" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#house_carousel" data-slide-to="0" class="active"></li>
+                <li data-target="#house_carousel" data-slide-to="1"></li>
+                <li data-target="#house_carousel" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="https://images.unsplash.com/photo-1570129477492-45c003edd2be?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max" alt="First slide">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Titolo 1</h5>
+                        <p>Descrizione 1</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="https://media.istockphoto.com/photos/beautiful-luxury-home-exterior-at-sunset-featuring-large-covered-picture-id1208206114?k=20&m=1208206114&s=612x612&w=0&h=kD-KC6BhYB77xoq_5lvDEJe6NrA9iL5r2-UAsFtig0I=" alt="Second slide">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Titolo 2</h5>
+                        <p>Descrizione 2</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="https://imagesvc.meredithcorp.io/v3/jumpstartpure/image?url=https://cf-images.us-east-1.prod.boltdns.net/v1/static/3281700261001/8d913bb0-aa78-460a-ad59-2603ff2491ea/894eda0e-b137-4eba-9328-6ecdc2040923/1280x720/match/image.jpg&w=1280&h=720&q=90&c=cc" alt="Third slide">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Titolo 3</h5>
+                        <p>Descrizione 3</p>
+                    </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#house_carousel" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#house_carousel" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
        
 
     </div>
