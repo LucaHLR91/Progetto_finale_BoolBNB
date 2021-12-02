30d7e45d119bb2de7f1f30f921e7d4fcc6f07476
Vlad <andreivladpaul@gmail.com>
2021-12-02 18:36:12 +0100

    dimensioni card

--- resources/views/admin/sponsorships/index.blade.php
+++ resources/views/admin/sponsorships/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.dashboard')
 @section('title', 'Sponsorizza')
-    
+
 
 
 @section('content')
@@ -8,32 +8,32 @@
 {{-- REINDIRIZZA ALLA PAGINA DEL PAGAMENTO  --}}
 <form action="">
     <div class="container">
-        <div class="row text-dark ">
+        <div class="row text-dark d-flex justify-content-center ">
             {{-- silver --}}
-            <div class="col-12 col-md-4">
-                <div class="card text-center p-5 card_width gradient-silver d-flex">
-                    <h3>Silver</h3>    
+            <div class="col-12 col-lg-4 col-md-9 mb-3">
+                <div class="gradient-silver card text-center card_width d-flex justify-content-center">
+                    <h3>Silver</h3>
                     <p>2,99 €</p>
                     <p>Per 24 ore in evidenza</p>
-                    <button type="submit" class="btn d-flex align-items-center justify-content-center mt-4"> Acquista ora <i class="fas fa-long-arrow-alt-right ms-3"></i></button>
+                    <button type="submit" class="btn d-flex align-items-center justify-content-center mt-2"> Acquista ora <i class="fas fa-long-arrow-alt-right ms-3"></i></button>
                 </div>
             </div>
             {{-- gold --}}
-            <div class="col-12 col-md-4">
-                <div class="card text-center p-5 card_width gradient-gold d-flex">
-                    <h3>Gold</h3>    
+            <div class="col-12 col-lg-4 col-md-9 mb-3">
+                <div class="gradient-gold card text-center card_width  d-flex justify-content-center">
+                    <h3>Gold</h3>
                     <p>5,99 €</p>
                     <p>Per 72 ore in evidenza</p>
-                    <button type="submit" class="btn d-flex align-items-center justify-content-center mt-4"> Acquista ora <i class="fas fa-long-arrow-alt-right ms-3"></i></button>
+                    <button type="submit" class="btn d-flex align-items-center justify-content-center mt-2"> Acquista ora <i class="fas fa-long-arrow-alt-right ms-3"></i></button>
                 </div>
             </div>
             {{-- platinum --}}
-            <div class="col-12 col-md-4">
-                <div class="card text-center p-5 card_width gradient-plat d-flex">
-                    <h3>Platinum</h3>    
+            <div class="col-12 col-lg-4 col-md-9 mb-3">
+                <div class="card text-center card_width gradient-plat d-flex justify-content-center">
+                    <h3>Platinum</h3>
                     <p>9,99 €</p>
                     <p>Per 144 ore in evidenza</p>
-                    <button type="submit" class="btn d-flex align-items-center justify-content-center mt-4"> Acquista ora <i class="fas fa-long-arrow-alt-right ms-3"></i></button>
+                    <button type="submit" class="btn d-flex align-items-center justify-content-center mt-2"> Acquista ora <i class="fas fa-long-arrow-alt-right ms-3"></i></button>
                 </div>
             </div>
         </div>
