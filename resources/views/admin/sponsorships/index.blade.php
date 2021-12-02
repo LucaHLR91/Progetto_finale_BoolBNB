b4823c4219c2af3383c44e7c2e8e63b408501e68
Edoardo <ercolino.edoardo@gmail.com>
2021-12-02 18:11:53 +0100

    card sponsorizzazioni

--- resources/views/admin/sponsorships/index.blade.php
+++ resources/views/admin/sponsorships/index.blade.php
@@ -1,62 +1,43 @@
 @extends('layouts.dashboard')
+@section('title', 'Sponsorizza')
+    
+
+
 @section('content')
 
-<div class="container text-dark justify-content-center">
-    <div class="row justify-content-between">
-        <div class=" justify-content-center pb-2">
-            <div class="col-4 position-relative">
-                <div class="card text-center p-5 card_width gradient-silver">
-             
-                    <h3>
-                    <strong>Silver</strong>
-                    </h3>    
-                    <p class="h1">2,99 €</p>
+{{-- REINDIRIZZA ALLA PAGINA DEL PAGAMENTO  --}}
+<form action="">
+    <div class="container">
+        <div class="row text-dark ">
+            {{-- silver --}}
+            <div class="col-12 col-md-4">
+                <div class="card text-center p-5 card_width gradient-silver d-flex">
+                    <h3>Silver</h3>    
+                    <p>2,99 €</p>
                     <p>Per 24 ore in evidenza</p>
-
-                    <div class="btn-container d-flex justify-content-center mt-4">
-                    <button type="button" class="btn">
-                        Acquista ora <i class="fas fa-long-arrow-alt-right ms-3"></i>
-                    </button>
-                    </div>
+                    <button type="submit" class="btn d-flex align-items-center justify-content-center mt-4"> Acquista ora <i class="fas fa-long-arrow-alt-right ms-3"></i></button>
                 </div>
             </div>
-        </div>
-        <div class=" justify-content-center pb-2 ">
-            <div class="col-4 position-relative">
-                <div class="card text-center p-5 gradient-gold card_width">
-             
-                    <h3>
-                    <strong>Gold</strong>
-                    </h3>   
-                    <p class="h1">5,99 €</p>
+            {{-- gold --}}
+            <div class="col-12 col-md-4">
+                <div class="card text-center p-5 card_width gradient-gold d-flex">
+                    <h3>Gold</h3>    
+                    <p>5,99 €</p>
                     <p>Per 72 ore in evidenza</p>
-
-                    <div class="btn-container d-flex justify-content-center mt-4">
-                    <button type="button" class="btn">
-                        Acquista ora <i class="fas fa-long-arrow-alt-right ms-3"></i>
-                    </button>
-                    </div>
+                    <button type="submit" class="btn d-flex align-items-center justify-content-center mt-4"> Acquista ora <i class="fas fa-long-arrow-alt-right ms-3"></i></button>
                 </div>
             </div>
-        </div>
-        <div class=" justify-content-center pb-2">
-            <div class="col-4 position-relative">
-                <div class="card text-center p-5 card_width gradient-plat">
-             
-                    <h3>
-                    <strong>Platinum</strong>
-                    </h3>   
-                    <p class="h1">9,99</p>
+            {{-- platinum --}}
+            <div class="col-12 col-md-4">
+                <div class="card text-center p-5 card_width gradient-plat d-flex">
+                    <h3>Platinum</h3>    
+                    <p>9,99 €</p>
                     <p>Per 144 ore in evidenza</p>
-
-                    <div class="btn-container d-flex justify-content-center mt-4">
-                    <button type="button" class="btn">
-                        Acquista ora <i class="fas fa-long-arrow-alt-right ms-3"></i>
-                    </button>
-                    </div>
+                    <button type="submit" class="btn d-flex align-items-center justify-content-center mt-4"> Acquista ora <i class="fas fa-long-arrow-alt-right ms-3"></i></button>
                 </div>
             </div>
         </div>
     </div>
-</div>
+</form>
+
 @endsection
