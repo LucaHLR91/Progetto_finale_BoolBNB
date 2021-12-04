2b863e7baafd6f2ae887d3fa3c8e25070ae30a14
robertoAureli <r.aureli@protonmail.com>
2021-12-04 19:32:00 +0100

    gestione backend ricerca completa con filtri

--- app/Http/Controllers/QueryController.php
+++ app/Http/Controllers/QueryController.php
@@ -8,44 +8,12 @@ use Illuminate\Http\Request;
 class QueryController extends Controller
 {
 
-
     public function index(Request $request)
     {
-        $beds = $request->beds;
-        $rooms = $request->rooms;
-        $city = ucfirst($request->city);
+        
 
-        $parameters = [
-            'beds' => $beds,
-            'rooms' => $rooms,
-            'city' => $city,
-        ];
-
-        if (!empty($beds) && !empty($rooms) && !empty($city)) {
-            $apartments = Apartment::filter(
-                ['beds' => ['>', $beds]],
-                ['rooms' => ['>', $rooms]],
-                ['city' => $city])->get();
-        } elseif (!empty($beds) && !empty($city)) {
-            $apartments = Apartment::filter(
-                ['beds' => ['>', $beds]],
-                ['city' => $city]
-            )->get();
-        } elseif (!empty($rooms) && !empty($city)) {
-            $apartments = Apartment::filter(
-                ['rooms' => ['>', $rooms]],
-                ['city' => $city]
-            )->get();
-        } elseif (!empty($city)) {
-            $apartments = Apartment::filter(
-                ['city' => $city]
-            )->get();
-        } else {
-            // Inserire app sponsorizzati
-            $apartments = Apartment::all();
-        }
-
-    
+        $apartments = Apartment::filter($request->all())->get();
+        
 
         $coordinates = array();
         foreach ($apartments as $apartment) {
@@ -55,7 +23,7 @@ class QueryController extends Controller
             );
         }
 
-        return view('guest.home.search', compact('apartments', 'coordinates', 'parameters'));
+        return view('guest.home.search', compact('apartments', 'coordinates'));
 
     }
 
