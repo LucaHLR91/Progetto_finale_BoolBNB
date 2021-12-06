9e06608c0b6eca356580357c17b7af75346a24b9
Vlad <andreivladpaul@gmail.com>
2021-12-06 16:19:18 +0100

    Fixe qunado admin non ha messaggi

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -6,18 +6,23 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                @foreach ($messages as $message)
-                    @if ($message->apartment_id == $apartment_id)
-                        <div class="mb-2 p-4 bg-secondary rounded">
-                            <h3>{{ $message->email }}</h3>
-                            <p>{{ $message->text }}</p>
-                            <small>{{ $message->date }}</small>
-                        </div>
 
-                    @else
-                        <h3 class="text-dark">Non ci sono messaggi</h3>
-                    @endif
-                @endforeach
+                @if ($messages->isEmpty())
+                    <h3 class="text-dark">Non ci sono messaggi</h3>
+                @else
+                    @foreach ($messages as $message)
+
+                        @if ($message->apartment_id == $apartment_id)
+
+                            <div class="mb-2 p-4 bg-secondary rounded">
+                                <h3>{{ $message->email }}</h3>
+                                <p>{{ $message->text }}</p>
+                                <small>{{ $message->date }}</small>
+                            </div>
+                        @endif
+                    @endforeach
+                @endif
+
             </div>
         </div>
     </div>
