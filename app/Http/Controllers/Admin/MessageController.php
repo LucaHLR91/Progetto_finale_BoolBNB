198bd27ad193e2fa5a016fb5098cca36ddbca99b
robertoAureli <r.aureli@protonmail.com>
2021-12-07 10:23:41 +0100

    file storage link img

--- app/Http/Controllers/Admin/MessageController.php
+++ app/Http/Controllers/Admin/MessageController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Apartment;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 use App\Message;
+use Illuminate\Support\Facades\Auth;
+
+
+
 
 class MessageController extends Controller
 {
@@ -16,6 +21,26 @@ class MessageController extends Controller
      */
     public function index(Request $request)
     {
+
+        $apartments = Apartment::where('user_id', auth()->user()->id)->get();
+
+        $messages = Message::whereIn('apartment_id', $apartments->pluck('id'))->get();
+
+        //insert  new messages for id = 18
+        
+        $message->push([
+            'apartment_id' => 18,
+            
+            'message' => 'Привет, как дела?',
+            'email' => 'rbizoon',
+         
+        ]);
+
+
+        dd($messages);
+
+        // query message with user id
+
         $messages = Message::all();
         $apartment_id = $request->id;
         return view('admin.messages.index', compact('messages', 'apartment_id'));
