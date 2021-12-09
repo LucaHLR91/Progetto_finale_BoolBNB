b09991cb227398b2bc6fad6ff7e22d385b9a99b9
Edoardo <ercolino.edoardo@gmail.com>
2021-12-09 16:42:49 +0100

    Primi accenni grafici nella sezione pagamento

--- resources/views/admin/payments/index.blade.php
+++ resources/views/admin/payments/index.blade.php
@@ -3,12 +3,14 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Braintree Payments</title>
-
+        
+        <title>Pagamento</title>
+        
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+        {{-- <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css"> --}}
 
+        {{-- collego l'app css --}}
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <!-- Styles -->
         <style>
             html, body {
@@ -79,31 +81,45 @@
                 </ul>
             </div>
         @endif
-        
-        <h1>Aggiungi la tua carta</h1>
-        <div class="flex-center position-ref full-height">
-            {{-- @dd($sponsorship_data) --}}
-            <div class="content">
-                <form method="post" id="payment-form" action="{{ route('admin.checkout') }}">
-                    @csrf
-                    @method('POST')
-                    <section>
-                        <label for="amount">
-                            <span class="input-label">Prezzo</span>
-                            <div class="input-wrapper amount-wrapper">
-                                <input id="amount" name="amount" type="number" step="any" min="1" placeholder="Prezzo"  value="{{ floatval($sponsorship_data['sponsorship_price'])  +  0.99 }}">
-                            </div>
-                        </label>
 
-                        <div class="bt-drop-in-wrapper">
-                            <div id="bt-dropin"></div>
-                        </div>
-                    </section>
-                    <input type="hidden" id="apartment_id" name="apartment_id" value="{{ $sponsorship_data['apartment_id'] }}">
-                    <input type="hidden" id="sponsorship_id" name="sponsorship_id" value="{{ $sponsorship_data['sponsorship_id'] }}">
-                    <input id="nonce" name="payment_method_nonce" type="hidden" />
-                    <button class="btn btn-primary" type="submit">Paga ora</button>
-                </form>
+        <div class="wrapper pt-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 text-center mb-5">
+                        <h1>Aggiungi la tua carta</h1>
+                    </div>
+                    <div class="col-12 text-center">
+                        <p>Stai acquistando la sponsorizzazione {{ucfirst($sponsorship_data['sponsorship_name'])}}</p>
+                    </div>
+                </div>
+            </div>
+            <div class="flex-center position-ref">
+                {{-- @dd($sponsorship_data)     --}}
+                <div class="content">
+                    <form method="post" id="payment-form" action="{{ route('admin.checkout') }}">
+                        @csrf
+                        @method('POST')
+                        <section>
+                            <label for="amount">
+                                <span class="input-label">Prezzo {{$sponsorship_data['sponsorship_price']}} €</span>
+                                <div class="input-wrapper amount-wrapper">
+                                    <input id="amount" name="amount" type="hidden" step="any" min="1" placeholder="Prezzo"  value="{{ floatval($sponsorship_data['sponsorship_price'])  +  0.99 }}">
+                                </div>
+                            </label>
+    
+                            <div class="bt-drop-in-wrapper">
+                                <div id="bt-dropin"></div>
+                            </div>
+                        </section>
+                        <input type="hidden" id="apartment_id" name="apartment_id" value="{{ $sponsorship_data['apartment_id'] }}">
+                        <input type="hidden" id="sponsorship_id" name="sponsorship_id" value="{{ $sponsorship_data['sponsorship_id'] }}">
+                        <input id="nonce" name="payment_method_nonce" type="hidden" />
+                        <button class="btn btn-primary" type="submit">Paga ora</button>
+                    </form>
+                </div>
+            </div>
+            <div class="col-12 mt-5 d-flex justify-content-end">
+                <img class="w-25" src="https://res.cloudinary.com/practicaldev/image/fetch/s--EZs7UUTE--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://thepracticaldev.s3.amazonaws.com/i/npiu494797ky06ir4nz0.png" alt="">
             </div>
         </div>
 
@@ -115,10 +131,8 @@
         braintree.dropin.create({
           authorization: client_token,
           selector: '#bt-dropin',
-          paypal: {
-            flow: 'vault'
-          }
-        }, function (createErr, instance) {
+        }, 
+        function (createErr, instance) {
           if (createErr) {
             console.log('Create Error', createErr);
             return;
