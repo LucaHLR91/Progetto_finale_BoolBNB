2fae3129a11e330d7950aa60e16569f5b18f65bc
robertoAureli <r.aureli@protonmail.com>
2021-12-10 23:36:26 +0100

    refactoring

--- app/Http/Controllers/QueryController.php
+++ app/Http/Controllers/QueryController.php
@@ -34,61 +34,40 @@ class QueryController extends Controller
 
     return view('guest.home.search', compact('apartments', 'coordinates', 'id_apartments', 'services'));
     }
-        
-    public function search(Request $request)
-    {
-        $geocoder = new GeoFunction(env('TOMTOM_API_KEY'));
-        $coordinates = $geocoder->geocodeAddress($request->city);
-        $geoapartments = Apartment::radius($coordinates['latitude'], $coordinates['longitude'], $request->radius)->get();
-        // Filtro gli appartamenti che soddisfano i filtri
-        $data = $request->except('services');
-        $apartments = Apartment::ignoreRequest(['radius'])->filter($data)->get();
-        // save common apartments in $apartments
-        $apartments = $apartments->intersect($geoapartments);
+
+    public function search (Request $request){
+        $id_apartments = $request->id_apartments;
+        $id_services = $request->services;
+
+        $results = DB::table('apartment_service')
+            ->select('apartment_id', 'service_id')
+            ->whereIn('apartment_id', $id_apartments)
+            ->whereIn('service_id', $id_services)
+            ->get();
+
         $coordinates = array();
         $id_apartments = array();
-        foreach ($apartments as $apartment) {
+
+        foreach ($results as $apartment) {
+            $id_apartments[] = $apartment->apartment_id;
+            $apartment = Apartment::find($apartment->apartment_id);
             $coordinates[] = array(
-                'latitude' => $apartment->latitude,
-                'longitude' => $apartment->longitude,
+                'latitude' => $apartment["latitude"],
+                'longitude' => $apartment["longitude"],
             );
-            $id_apartments[] = $apartment->id;
         }
 
+        $apartments = Apartment::whereIn('id', $id_apartments)->get();
         $services = Service::all();
 
         return view('guest.home.search', compact('apartments', 'coordinates', 'id_apartments', 'services'));
+
     }
+        
+ 
 
   
 
-    // function to filter apartments_id  by services
 
-    public function getApartmentByServices($apartments_id, $id_service)
-    {
-
-        $apartmentByServices = DB::table('apartment_service')
-            ->join('services', 'apartment_service.service_id', '=', 'services.id')
-            ->join('apartments', 'apartment_service.apartment_id', '=', 'apartments.id')
-            ->select('apartments.id', 'services.service_name')
-            ->whereIn('apartment_service.service_id', [$id_service])
-            ->whereIn('apartment_service.apartment_id', [$apartments_id])
-            ->get();
-
-        return $apartmentByServices;
-    }
-
-    public function getServicesByApartment(Request $request)
-    {
-        $apartment_id = $request->input('apartment_id');
-        $services = DB::table('apartment_service')
-            ->join('services', 'apartment_service.service_id', '=', 'services.id')
-            ->join('apartments', 'apartment_service.apartment_id', '=', 'apartments.id')
-            ->select('apartments.id', 'services.service_name', 'services.id')
-            ->where('apartment_id', $apartment_id)
-            ->get();
-        return response()->json($services);
-
-    }
 
 }
