94c9f62f2fb8996aa48756bf286f156013460ad9
Edoardo <ercolino.edoardo@gmail.com>
2021-12-11 16:40:43 +0100

    card degli appartamenti

--- resources/views/guest/home/search.blade.php
+++ resources/views/guest/home/search.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
 
-    {{-- il seguente link serve per vedere cosa si è cercato --}}
+    {{-- il seguente dic serve per vedere cosa si è cercato (passare nel compact la variabile parameters)--}}
     {{-- <div class="container-fluid text-dark mb-4">
         <div class="row">
             <div class="col-12 d-flex justify-content-center">
@@ -13,11 +13,12 @@
         </div>
     </div> --}}
     <div class="container-fluid text-dark h-600">
-        <form action="{{ route('searchQuery') }}" method="get">
+
+        <form action="{{ route('searchQuery') }}" method="get" class="d-flex justify-content-center">
 
 
             <div class="form-group">
-                <h4>Filtra per servizi:</h4>
+                <h3 class="text-center mb-4 mt-2">Filtra per servizi:</h3>
 
                 @foreach ($id_apartments as $id_apartment)
                     <input name="id_apartments[]" id="id_apartments" value="{{ $id_apartment }}" type="hidden">
@@ -41,7 +42,7 @@
 
             @if($apartments->isEmpty())
             <div class="col-12 ">
-                <div class="mb-4 h-200 text-center ">
+                <div class="my-5 text-center ">
                     <h4>Al momento non ci sono appartamenti </h4>
                 </div>
 
@@ -50,26 +51,57 @@
 
             @else
                 @foreach ($apartments as $result)
-                <div class="col-lg-4 col-12">
-                    {{-- <a class="text-decoration-none" href="{{route('messages.show', $result->id) }}"> --}}
+                <div class="col-lg-4 col-md-6 col-12">
+                    <a class="text-decoration-none" href="{{route('messages.show', $result->id) }}">
+                        {{-- @dd($result) --}}
+                        <div class="card text-dark mb-3" >
+                            <img src="{{ asset('storage/image_apartments/' . $result->image) }}" class="card-img-top" style="width: 100%; height: 300px">
+                            <h4 class="text-center my-2">{{ $result->title }}</h4>
+                            <div class="w-100 d-flex flex-wrap">
+                                <div class="spazio4">
+                                    <p class=" text-center">N° Stanze: {{ $result->rooms }}</p>
+                                </div>
+                                <div class="spazio4">
+                                    <p class=" text-center">N° Letti: {{ $result->beds }}</p>
+                                </div>
+                                <div class="spazio4">
+                                    <p class=" text-center">N° Bagni: {{ $result->bathrooms }}</p>
+                                </div>
+                                <div class="spazio4">
+                                    <p class=" text-center">MQ: {{ $result->square_meters }}</p>
+                                </div>
+                                <div class="w-50">
+                                    <p class=" text-center">Città: {{ $result->city }}</p>
+                                </div>
+                                <div class="w-50">
+                                    <p class=" text-center">Indirizzo: {{ $result->address }}</p>
+                                </div>
+                                <div class="w-100">
+                                    {{-- effettuare un if per far apparire la disponibilità --}}
+                                    @if ($result->avaliability == 1)
+                                        <p class=" text-center">Al momento è disponibile</p>
+                                    @endif
+                                </div>
+                            </div>
+                        </div>
                         {{-- RIMUOVERE LE CLASSI DEI BORDI --}}
                         {{-- @dd($result) --}}
-                        <div class="house h-250 mb-4 d-flex border ">
-                            <div class="col-6 text-dark py-2 ">
+                        {{-- <div class="house mb-4 d-flex border h-250">
+
+                            <div class="col-5 text-dark py-2">
                                 <h3 class="m-0">{{ $result->title }}</h3>
                                 <p class="m-0">N° Stanze: {{ $result->rooms }}</p>
-                                <p class="m-0">N° Letti:{{ $result->beds }}</p>
-                                <p class="m-0">N° Bagni:{{ $result->bathrooms }}</p>
-                                <p class="m-0">MQ:{{ $result->square_meters }}</p>
-                                <p class="m-0">Città:{{ $result->city }}</p>
-                                <p class="m-0">Indirizzo:{{ $result->address }}</p>
+                                <p class="m-0">N° Letti: {{ $result->beds }}</p>
+                                <p class="m-0">N° Bagni: {{ $result->bathrooms }}</p>
+                                <p class="m-0">MQ: {{ $result->square_meters }}</p>
+                                <p class="m-0">Città: {{ $result->city }}</p>
+                                <p class="m-0">Indirizzo: {{ $result->address }}</p>
                             </div>
-                            <div class="col-6 p-0 border ">
-                                {{-- <img src="{{ asset('storage/image_apartments/1638871398.jpeg') }}" alt="" title="" style="width:200px; height:200px"> --}}
-                                <img src="{{ asset('storage/image_apartments/' . $result->image) }}" alt="" class="w-100 h-100">
-                            </div>
+                            <div class="col-7 p-0"> --}}
+                                {{-- <img src="{{ asset('storage/image_apartments/' . $result->image) }}" alt="" class="w-100 h-100"> --}}
+                            {{-- </div> --}}
                             {{-- <a href="{{route('messages.show', $result->id) }}" class="btn btn-info">Visualizza appartamento</a> --}}
-                        </div>
+                        {{-- </div> --}}
                     </a>
                 </div>
                 @endforeach
